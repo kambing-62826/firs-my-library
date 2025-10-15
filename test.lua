c38d2817bbf950ca5aa0b275600954c56b7b725c
YuviHub <yunusnoob123@gmail.com>
2025-10-16 03:56:39 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1615,7 +1615,7 @@ function TabAPI:createColorPicker(config)
     local Frame = Instance.new("Frame")
     Frame.Name = "Colorpicker"
     Frame.Size = UDim2.new(1, 0, 0, FRAME_HEIGHT_COLLAPSED)
-    Frame.BackgroundColor3 = Color3.fromRGB(64, 68, 75)
+    Frame.BackgroundColor3 = Theme.MainBG
     Frame.ClipsDescendants = true
     Frame.Parent = parent
 
