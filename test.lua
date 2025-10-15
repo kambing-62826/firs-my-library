fbd848d61a138629feb54df2839525d344913e53
YuviHub <yunusnoob123@gmail.com>
2025-10-16 03:18:00 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1942,6 +1942,23 @@ end
             StopDrag()
         end
     end)
+    
+        -- Expand/collapse
+    ColorpickerBtn.MouseButton1Click:Connect(function()
+        ColorPickerToggled = not ColorPickerToggled
+        if ColorPickerToggled then
+            ColorSelection.Visible = true
+            HueSelection.Visible = true
+            -- Menggunakan lebar 1 (100%) dan ketinggian expanded
+            Frame:TweenSize(UDim2.new(1, 0, 0, FRAME_HEIGHT_EXPANDED),"Out","Quart",0.2,true)
+        else
+            ColorSelection.Visible = false
+            HueSelection.Visible = false
+            -- Menggunakan lebar 1 (100%) dan ketinggian collapsed
+            Frame:TweenSize(UDim2.new(1, 0, 0, FRAME_HEIGHT_COLLAPSED),"Out","Quart",0.2,true)
+        end
+    end)
+end
 
     -- createKeybind
     function TabAPI:createKeybind(config)
